223561696ca417bd1c839439ab16ec583fd9826d
Nanask <ybhag1005@daum.net>
2022-01-10 23:59:25 +0900

    start

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,15 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    /*
+    1. 인사말 출력
+    2. 사용자 이름 받기
+    3. 입력 받은 사용자 이름 출력
+     */
+    println("안녕하세요!")
+    println("쇼핑을 계속 하시려면 이름을 입력해주세요")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    val name = readLine()
+    println("""
+        반갑습니다. $name 님
+    """.trimIndent())
+    //trimIndent() 개행으로 생기는 부분을 없애주는것
 }
